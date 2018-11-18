148b310aefab953aaf38899bd3c2f81343e49b2c
jsa <you@example.com>
2018-11-18 21:19:21 +0000

    first commit

---
+++ src/resources/js/app.js
@@ -0,0 +1,18 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('home', require('./components/Home.vue'));
+Vue.component('navbar', require('./components/Navbar.vue'));
+Vue.component('bottombar', require('./components/Bottombar.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
